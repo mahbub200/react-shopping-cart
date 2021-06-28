c7b68c6c4a585a88dc98e738092cb0b34f1c4c30
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2021-06-28 21:16:22 +0600

    product with images added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@
  // chanes to feature 1 done
 
 import React from 'react'
-function App() {
-  return (
-    <div className = "grid-container">
-      <header>
-        <a href ="/">React shopping cart</a>
-        </header>
+import  data from "./data.json";
+import Products from "./components/Products";
 
-        <main>
-          Product list
-        </main>
-        <footer>
-          All right is reserved
-        </footer>
-        </div>);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+ render(){
+   return (
+  <div className = "grid-container">
+    <header>
+      <a href ="/">React shopping cart</a>
+      </header>
+
+      <main>
+      <div className="content">
+            <div className="main">
+            <Products products={this.state.products}></Products>
+              products
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+      </main>
+      <footer>
+        All right is reserved
+      </footer>
+      </div>);
 }
+} 
 export default App;
