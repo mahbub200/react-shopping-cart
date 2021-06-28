aae4743058d32bce2718f5f60281eb3caaf602b8
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2021-06-28 15:59:48 +0600

    creating header and footer template

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+function App() {
+  return (
+    <div className = "grid-container">
+      <header>
+        <a href ="/">React shopping cart</a>
+        </header>
+
+        <main>
+          Product list
+        </main>
+        <footer>
+          All right is reserved
+        </footer>
+        </div>);
+}
+export default App;
